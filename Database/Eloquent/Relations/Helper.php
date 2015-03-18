4a1799f6c6dd465ff44cd1ef92081fc88f70b688
Tom Rochette <roctom@gmail.com>
2015-03-17 23:18:50 -0400

    Refactor Exolnet\Database\Eloquent\Relations\Helper::syncHasMany.
    Delete all keys not part of the update upfront, query all related models once and then update them. This way, we prevent the fetching of models one by one using Exolnet\Database\Eloquent\Relations\Helper::getRelatedModel.

--- Database/Eloquent/Relations/Helper.php
+++ Database/Eloquent/Relations/Helper.php
@@ -21,33 +21,36 @@ class Helper
 
 	protected static function syncHasManyInternal(HasMany $relation, array $items, Closure $onSave = null)
 	{
-		$updated_keys = [];
-		$new_items    = [];
+		$newItems    = [];
+		$related     = $relation->getRelated();
+		$keyName     = $related->getKeyName();
 
-		// 1. Update existing relations and prepare the delete and create steps
+		// 1. Extract all updated keys
+		$updatedKeys = Arr::pluck($items, $keyName);
+
+		// 2. Delete old relations
+		static::deleteAllRelatedExcept($relation, $updatedKeys);
+
+		// 3. Update existing relations
+		$models = $relation->all();
 		foreach ($items as $item) {
-			$model = static::getRelatedModel($relation, $item);
+			$model = $models->find(array_get($item, $keyName));
 
 			if ($model === null) {
-				$new_items[] = $item;
+				$newItems[] = $item;
 				continue;
 			}
 
-			// On met à jour la relation
+			// Update relation data
 			$model->fill($item)->save();
 
 			if ($onSave) {
 				$onSave($model, $item);
 			}
-
-			$updated_keys[] = $model->getKey();
 		}
 
-		// 2. Delete old relations
-		static::deleteAllRelatedExcept($relation, $updated_keys);
-
-		// 3. Create new relations
-		foreach ($new_items as $item) {
+		// 4. Insert new relations
+		foreach ($newItems as $item) {
 			$model = $relation->create($item);
 
 			if ($onSave) {
@@ -58,34 +61,16 @@ class Helper
 
 	/**
 	 * @param Relation $relation
-	 * @param array    $item
-	 * @return \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null|static
+	 * @param array    $excludedIds
 	 */
-	protected static function getRelatedModel(Relation $relation, array $item)
+	public static function deleteAllRelatedExcept(Relation $relation, $excludedIds = [])
 	{
-		$related  = $relation->getRelated();
-		$key_name = $related->getKeyName();
-		$key      = array_get($item, $key_name, null);
+		$related = $relation->getRelated();
+		$keyName = $related->getKeyName();
+		$query   = $relation->getQuery();
 
-		if ($key === null) {
-			return null;
-		}
-
-		return $related->find($key);
-	}
-
-	/**
-	 * @param Relation $relation
-	 * @param array    $excluded_ids
-	 */
-	public static function deleteAllRelatedExcept(Relation $relation, $excluded_ids = [])
-	{
-		$related  = $relation->getRelated();
-		$key_name = $related->getKeyName();
-		$query    = $relation->getQuery();
-
-		if (count($excluded_ids) > 0) {
-			$query->whereNotIn($key_name, $excluded_ids);
+		if (count($excludedIds) > 0) {
+			$query->whereNotIn($keyName, $excludedIds);
 		}
 
 		$query->delete();
